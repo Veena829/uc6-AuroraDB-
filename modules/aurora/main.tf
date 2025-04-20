8ed4e1a2e27b7dfd171d780fa41c5f2a6cb97fba
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-20 13:32:33 +0530

    Update main.tf

--- modules/aurora/main.tf
+++ modules/aurora/main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "aurora_sg" {
 }
 
 resource "aws_db_subnet_group" "aurora_subnet" {
-  name       = "aurora-subnet-group1"
+  name       = "aurora-subnet-group2"
   subnet_ids = var.subnet_ids
 }
 
