0ee1ef8ec4c7e993fee2fcfd1322850f66ddf56a
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-20 16:59:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "aurora" {
 }
 
 resource "aws_secretsmanager_secret" "aurora_credentials" {
-  name = "aurora-db-credentials-v4"
+  name = "aurora-db-credentials-v5"
 }
 
 resource "aws_secretsmanager_secret_version" "aurora_credentials_version" {
